4f28c4aaf1afe74392b7b3042e440c08a707f1f4
practice_dailytrial-admin <mamofy612@gmail.com>
2025-12-01 17:40:12 +0900

    記事を更新できるよう変更

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
   # get '/' => 'home#index'
   # get "/about" => "home#about"
 
-  resources :articles, only: [:show, :new, :create]
+  resources :articles, only: [:show, :new, :create, :edit, :update]
 
 end

36aed747b8b7a97332a774f0b67a289f452c61cf
practice_dailytrial-admin <mamofy612@gmail.com>
2025-12-10 21:53:14 +0900

    formを部分テンプレートに変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update]
+
     def index
         # render 'article/index'
         # @title = "デイトラ"
@@ -9,7 +11,6 @@ class ArticlesController < ApplicationController
     # end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -27,11 +28,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
            redirect_to article_path(@article), notice: '更新できました'
         else
@@ -49,5 +48,9 @@ class ArticlesController < ApplicationController
     private
     def article_params
         params.require(:article).permit(:title, :content)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
     end
 end
